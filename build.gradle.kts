01b973dff2a018e01507034c400bd26141ff8aad
Anonymous <3530749992@qq.com>
2026-02-12 23:35:20 +0800

    Optimized some dependencies, simplified the gladle-doc format.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,38 @@
 plugins {
     id("java")
-    id("org.springframework.boot") version "3.3.7"
-    id("io.spring.dependency-management") version "1.1.7"
+    id("org.springframework.boot") version "4.0.1"
 }
 
 group = "com.anonymous"
 version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.mybatis:mybatis-spring:3.0.3")
+    // 使用spring平台，统一依赖版本
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:4.0.1"))
 
-    // Source: https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
     implementation("org.springframework.boot:spring-boot-starter-web")
-
-    // Source: https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-jdbc
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
 
-    // Source: https://mvnrepository.com/artifact/com.baomidou/mybatis-plus-boot-starter
-    implementation("com.baomidou:mybatis-plus-boot-starter:3.5.7")
+    // Source: https://mvnrepository.com/artifact/org.mybatis.spring.boot/mybatis-spring-boot-starter
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:4.0.0")
 
     // Source: https://mvnrepository.com/artifact/com.mysql/mysql-connector-j
     runtimeOnly("com.mysql:mysql-connector-j")
 
-    // Source: https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-data-redis
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
-
-    // Source: https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-amqp
-    implementation("org.springframework.boot:spring-boot-starter-amqp")
-
-    // Source: https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-websocket
-    implementation("org.springframework.boot:spring-boot-starter-websocket")
-
-    // Source: https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
